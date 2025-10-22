4bbd4c8331ede161e5e7bba8297e1fc1f2a4807d
layyliss <layjaysalaco@gmail.com>
2025-10-21 23:23:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ const productos = [
       nombre: "Barbie en El Cascanueces",
       descripcion: "La primera película animada de Barbie, donde interpreta a Clara en una mágica aventura con el Príncipe Cascanueces.",
       precio: 129.99,
-      imagen: "img1.PNG"
+      imagen: "img/img1.PNG"
     },
     {
       nombre: "Barbie como Rapunzel",
       descripcion: "Barbie da vida a Rapunzel, una artista con un pincel mágico que pinta la libertad.",
       precio: 139.99,
-      imagen: "img2.PNG"
+      imagen: "img/img2.PNG"
     },
     {
       nombre: "Barbie y el Lago de los Cisnes",
       descripcion: "Basada en el ballet clásico, Barbie se transforma en Odette y enfrenta al malvado Rothbart.",
       precio: 149.99,
-      imagen: "img3.PNG"
+      imagen: "img/img3.PNG"
     },
     {
       nombre: "Barbie Princesa y la Plebeya",
       descripcion: "Dos chicas idénticas, una princesa y una plebeya, descubren el poder de la amistad y la libertad.",
       precio: 159.99,
-      imagen: "img4.PNG"
+      imagen: "img/img4.PNG"
     }
   ];
   
@@ -63,3 +63,4 @@ const productos = [
   window.onload = renderizarCatalogo;
 
   
+
